2efda59a69cb807256e1f53581f81406d891d4fb
Gian Tricarico <gtricarico@stuy.edu>
2017-09-18 10:14:54 -0400

    add comments and print statement

--- Problem1.c
+++ Problem1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// calculates the sum of all the natural numbers below limit that are multiples
+// of x
 int sumofMults(int x, int limit)
 {
   int sum = 0;
@@ -13,6 +15,7 @@ int sumofMults(int x, int limit)
 
 int main()
 {
+  printf("The sum of all the multiples of 3 or 5 below 10 is: %d\n", sumofMults(3, 10) + sumofMults(5, 10));
   printf("The sum of all the multiples of 3 or 5 below 1000 is: %d\n", sumofMults(3, 1000) + sumofMults(5, 1000));
   return 0;
 }
