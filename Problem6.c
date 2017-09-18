ced14bd1d8de6da1ac27d14603c55c0379583a39
Gian Tricarico <gtricarico@stuy.edu>
2017-09-18 10:05:46 -0400

    Do problem 6

---
+++ Problem6.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int sumOfSquares(int x)
+{
+  int i = 1;
+  int retVal = 0;
+  while (i <= x) {
+    retVal += i * i;
+    i++;
+  }
+  return retVal;
+}
+
+int squareOfSum(int x)
+{
+  int i = 1;
+  int retVal = 0;
+  while (i <= x) {
+    retVal += i;
+    i++;
+  }
+  retVal = retVal * retVal;
+  return retVal;
+}
+
+int main()
+{
+  printf("The difference between the sum of the squares of the first ten natural numbers and the square of the sum is: %d\n", squareOfSum(10) - sumOfSquares(10));
+  printf("The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is: %d\n", squareOfSum(100) - sumOfSquares(100));
+  return 0;
+}
